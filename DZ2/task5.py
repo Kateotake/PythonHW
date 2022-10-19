46722632d6430c1dc10c436c46a36e156279ef62
KuznetsovaE <kettt56@mail.ru>
2022-10-19 23:20:37 +0300

    HomeW2

--- DZ2/task5.py
+++ DZ2/task5.py
@@ -1,5 +1,5 @@
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
-with open('Sem/file.txt', 'r') as my_file:
+with open('DZ2/file.txt', 'r') as my_file:
     n = int(my_file.readline(2)) * int(my_file.readline(4))
 print(n)
